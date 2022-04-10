241b9624cb8d3c09c55f1aa70377e9b723f48711
Prashant Kumar <54999940+prashant-kr6@users.noreply.github.com>
2022-04-10 22:27:39 +0530

    Add files via upload

--- create.blade.php
+++ create.blade.php
@@ -6,48 +6,27 @@
                     <!-- DataTales Example -->
                     <div class="card shadow mb-4">
                         <div class="card-header py-3">
-                            <h6 class="m-0 font-weight-bold text-primary">Add Customer
-                                <a href="{{url('admin/customer')}}" class="float-right btn btn-success btn-sm">View All</a>
+                            <h6 class="m-0 font-weight-bold text-primary">Add Department
+                                <a href="{{url('admin/department')}}" class="float-right btn btn-success btn-sm">View All</a>
                             </h6>
                         </div>
                         <div class="card-body">
-
-                            @if($errors->any())
-                                @foreach($errors->all() as $error)
-                                    <p class="text-danger">{{$error}}</p>
-                                @endforeach
-                            @endif
-
                             @if(Session::has('success'))
                             <p class="text-success">{{session('success')}}</p>
                             @endif
                             <div class="table-responsive">
-                                <form method="post" enctype="multipart/form-data" action="{{url('admin/customer')}}">
+                                <form method="post" action="{{url('admin/department')}}">
                                     @csrf
-                                    <table class="table table-bordered" >
+                                    <table class="table table-bordered">
                                         <tr>
-                                            <th>Full Name <span class="text-danger">*</span></th>
-                                            <td><input name="full_name" type="text" class="form-control" /></td>
+                                            <th>Title</th>
+                                            <td><input name="title" type="text" class="form-control" /></td>
                                         </tr>
                                         <tr>
-                                            <th>Email <span class="text-danger">*</span></th>
-                                            <td><input name="email" type="email" class="form-control" /></td>
-                                        </tr>
-                                        <tr>
-                                            <th>Password <span class="text-danger">*</span></th>
-                                            <td><input name="password" type="password" class="form-control" /></td>
-                                        </tr>
-                                        <tr>
-                                            <th>Mobile <span class="text-danger">*</span></th>
-                                            <td><input name="mobile" type="text" class="form-control" /></td>
-                                        </tr>
-                                        <tr>
-                                            <th>Photo</th>
-                                            <td><input name="photo[]" multiple type="file" /></td>
-                                        </tr>
-                                        <tr>
-                                            <th>Address</th>
-                                            <td><textarea name="address" class="form-control"></textarea></td>
+                                            <th>Detail</th>
+                                            <td>
+                                                <textarea name="detail" class="form-control"></textarea>
+                                            </td>
                                         </tr>
                                         <tr>
                                             <td colspan="2">
--- index.blade.php
+++ index.blade.php
@@ -6,8 +6,8 @@
                     <!-- DataTales Example -->
                     <div class="card shadow mb-4">
                         <div class="card-header py-3">
-                            <h6 class="m-0 font-weight-bold text-primary">Customers
-                                <a href="{{url('admin/customer/create')}}" class="float-right btn btn-success btn-sm">Add New</a>
+                            <h6 class="m-0 font-weight-bold text-primary">Department
+                                <a href="{{url('admin/department/create')}}" class="float-right btn btn-success btn-sm">Add New</a>
                             </h6>
                         </div>
                         <div class="card-body">
@@ -19,18 +19,14 @@
                                     <thead>
                                         <tr>
                                             <th>#</th>
-                                            <th>FullName</th>
-                                            <th>Email</th>
-                                            <th>Mobile</th>
+                                            <th>Title</th>
                                             <th>Action</th>
                                         </tr>
                                     </thead>
                                     <tfoot>
                                         <tr>
                                             <th>#</th>
-                                            <th>FullName</th>
-                                            <th>Email</th>
-                                            <th>Mobile</th>
+                                            <th>Title</th>
                                             <th>Action</th>
                                         </tr>
                                     </tfoot>
@@ -39,13 +35,11 @@
                                             @foreach($data as $d)
                                             <tr>
                                                 <td>{{$d->id}}</td>
-                                                <td>{{$d->full_name}}</td>
-                                                <td>{{$d->email}}</td>
-                                                <td>{{$d->mobile}}</td>
+                                                <td>{{$d->title}}</td>
                                                 <td>
-                                                    <a href="{{url('admin/customer/'.$d->id)}}" class="btn btn-info btn-sm"><i class="fa fa-eye"></i></a>
-                                                    <a href="{{url('admin/customer/'.$d->id).'/edit'}}" class="btn btn-primary btn-sm"><i class="fa fa-edit"></i></a>
-                                                    <a onclick="return confirm('Are you sure to delete this data?')" href="{{url('admin/customer/'.$d->id).'/delete'}}" class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></a>
+                                                    <a href="{{url('admin/department/'.$d->id)}}" class="btn btn-info btn-sm"><i class="fa fa-eye"></i></a>
+                                                    <a href="{{url('admin/department/'.$d->id).'/edit'}}" class="btn btn-primary btn-sm"><i class="fa fa-edit"></i></a>
+                                                    <a onclick="return confirm('Are you sure to delete this data?')" href="{{url('admin/department/'.$d->id).'/delete'}}" class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></a>
                                                 </td>
                                             </tr>
                                             @endforeach

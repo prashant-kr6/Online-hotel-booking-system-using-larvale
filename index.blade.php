d319f0ecdc7a12a0afd8a430173e4794d77fddfe
Prashant Kumar <54999940+prashant-kr6@users.noreply.github.com>
2022-04-10 22:26:19 +0530

    Add files via upload

--- index.blade.php
+++ index.blade.php
@@ -6,8 +6,8 @@
                     <!-- DataTales Example -->
                     <div class="card shadow mb-4">
                         <div class="card-header py-3">
-                            <h6 class="m-0 font-weight-bold text-primary">Customers
-                                <a href="{{url('admin/banner/create')}}" class="float-right btn btn-success btn-sm">Add New</a>
+                            <h6 class="m-0 font-weight-bold text-primary">All Bookings
+                                <a href="{{url('admin/booking/create')}}" class="float-right btn btn-success btn-sm">Add New</a>
                             </h6>
                         </div>
                         <div class="card-body">
@@ -19,36 +19,40 @@
                                     <thead>
                                         <tr>
                                             <th>#</th>
-                                            <th>Banner</th>
-                                            <th>Alt Text</th>
-                                            <th>Status</th>
+                                            <th>Customer</th>
+                                            <th>Room No.</th>
+                                            <th>Room Type</th>
+                                            <th>CheckIn Date</th>
+                                            <th>CheckOut Date</th>
+                                            <th>Ref</th>
                                             <th>Action</th>
                                         </tr>
                                     </thead>
                                     <tfoot>
                                         <tr>
                                             <th>#</th>
-                                            <th>Banner</th>
-                                            <th>Alt Text</th>
-                                            <th>Status</th>
+                                            <th>Customer</th>
+                                            <th>Room No.</th>
+                                            <th>Room Type</th>
+                                            <th>CheckIn Date</th>
+                                            <th>CheckOut Date</th>
+                                            <th>Ref</th>
                                             <th>Action</th>
                                         </tr>
                                     </tfoot>
                                     <tbody>
-                                        @if($data)
-                                            @foreach($data as $d)
-                                            <tr>
-                                                <td>{{$d->id}}</td>
-                                                <td><img width="100" src="{{asset('storage/app/'.$d->banner_src)}}" /></td>
-                                                <td>{{$d->alt_text}}</td>
-                                                <td>{{$d->publish_status}}</td>
-                                                <td>
-                                                    <a href="{{url('admin/banner/'.$d->id).'/edit'}}" class="btn btn-primary btn-sm"><i class="fa fa-edit"></i></a>
-                                                    <a onclick="return confirm('Are you sure to delete this data?')" href="{{url('admin/banner/'.$d->id).'/delete'}}" class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></a>
-                                                </td>
-                                            </tr>
-                                            @endforeach
-                                        @endif
+                                        @foreach($data as $booking)
+                                        <tr>
+                                            <td>{{$booking->id}}</td>
+                                            <td>{{$booking->customer->full_name}}</td>
+                                            <td>{{$booking->room->title}}</td>
+                                            <td>{{$booking->room->Roomtype->title}}</td>
+                                            <td>{{$booking->checkin_date}}</td>
+                                            <td>{{$booking->checkout_date}}</td>
+                                            <td>{{$booking->ref}}</td>
+                                            <td><a href="{{url('admin/booking/'.$booking->id.'/delete')}}" class="text-danger" onclick="return confirm('Are you sure you want to delete this data?')"><i class="fa fa-trash"></i></a></td>
+                                        </tr>
+                                        @endforeach
                                     </tbody>
                                 </table>
                             </div>
